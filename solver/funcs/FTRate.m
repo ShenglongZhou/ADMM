4dc959eae92f98fe62085e2c6b8b90f46f6d86bc
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-07-05 21:12:38 +0100

    Delete FTRate.m

--- solver/funcs/FTRate.m
+++
@@ -1,8 +0,0 @@
-function [FPR,TPR]=FTRate(A0,A)
- 
-eps   = 1e-4;
-Index = find(abs(A0)>eps);
-FPR   = length(find(abs(A(Index))<=eps))/length(find(abs(A)<=eps));
-TPR   = length(find(abs(A(Index))>eps))/length(find(abs(A)>eps));
-
-end 
