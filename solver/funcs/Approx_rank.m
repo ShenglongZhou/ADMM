7d44105e8c47877265a2cf8d3bdf1f5e7e1433f0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-07-05 21:12:23 +0100

    Delete Approx_rank.m

--- solver/funcs/Approx_rank.m
+++
@@ -1,8 +0,0 @@
-function [Arank] = Approx_rank(A)
-     p    = size(A,1);
-    [~,D] = svd(A); 
-    for k = 2:p
-        if D(k,k)/D(1,1)<0.01; break; end
-    end
-    if k == p;  Arank=p; else; Arank=k-1; end
-end
